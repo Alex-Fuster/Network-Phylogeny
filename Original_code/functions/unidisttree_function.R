cb3d91004853c7a1e9319f2fa3fd2ccba1f32e62
Alex-Fuster <alexfuster7@gmail.com>
2023-06-24 11:32:44 -0400

    commit

--- Original_code/functions/unidisttree_function.R
+++
@@ -1,7 +0,0 @@
-unidisttree <- function(distance_matirx){
-  tree <- ape::as.phylo(hclust(as.dist(distance_matrix)))
-  uni_edge_length <- rep(1, length(tree$edge.length))
-  tree$edge.length = 1
-}
-
-## FONCTIONNE PAS ! CAR LES TIPS NE CORRESPONDENT PAS AUX ESPECES, DONC LA MATRICE DE DISTANCES OBTENUE N'A RIEN à VOIR DES LA MATRICE DE DISTANCE DES TRAITS
